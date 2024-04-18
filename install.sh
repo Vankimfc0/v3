fac02e564d1bc3576eef32e2a64f53bc2afae580
Vankimfc0 <108604777+Vankimfc0@users.noreply.github.com>
2024-04-18 17:24:02 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-clear && cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/Vankimfc0/v3/blob/main/v3.zip && unzip v3.zip && chmod +x * && mv mod login && login
+clear && cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/Vankimfc0/v3/raw/main/v3.zip && unzip v3.zip && chmod +x * && mv mod login && login
